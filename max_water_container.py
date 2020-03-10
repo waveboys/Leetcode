2e80fcc4f0b0c408bade2fb5bf6b822891240450
Lambert <lambert@LambertdeMacBook-Air.local>
2020-03-10 11:08:09 +0800

    2020-03-08 2道题目

--- max_water_container.py
+++ max_water_container.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 #@Time  : 2020/3/8 下午6:30
-#@Author: guangliang
 #@File  : max_water_container.py
 """
 Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
