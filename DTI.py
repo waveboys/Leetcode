64a4b8b312d0cf5d4ddcd639997abd2401813fc4
Lambert <lambert@LambertdeMacBook-Air.local>
2020-03-24 15:49:51 +0800

    2020-03-24 新增Divide Two Integers

--- DTI.py
+++ DTI.py
@@ -21,6 +21,11 @@ Input: dividend = 7, divisor = -3
 Output: -2
 Explanation: 7/-3 = truncate(-2.33333..) = -2.
 """
+
+"""
+思路1：计算两个数的位数之差，除数*10的位数差之幂来不断逼近被除数
+思路2：通过的位的运算
+"""
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         if dividend == -2147483648 and divisor == -1:
@@ -50,4 +55,37 @@ class Solution:
             s += ss
             dividend -= divisor*ss
         return sign * s
+
+    def divide1(self, dividend: int, divisor: int) -> int:
+        # edge case overflow
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+
+        if dividend == 0:
+            return 0
+
+        if divisor == 1:
+            return dividend
+
+        sign = 1
+        if (dividend < 0) ^ (divisor < 0):
+            sign = -1
+
+        v = abs(dividend)
+        s = abs(divisor)
+
+        so_far = 0
+        quot = 0
+        # find quotient bitwise starting from highest bit
+        for i in range(31, -1, -1):
+            if so_far + (s << i) <= v:
+                so_far += (s << i)
+                quot |= (1 << i)
+
+        if sign == -1:
+            return -quot
+        else:
+            return quot
+
+
 print(Solution().divide(2147483648,1))
